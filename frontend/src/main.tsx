2c58b3bddf6e7aac64b7ed3bcdeea18f40a280a9
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-02-11 16:55:28 +0000

    update/add-missing-folders

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.querySelector("#root")!);
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
