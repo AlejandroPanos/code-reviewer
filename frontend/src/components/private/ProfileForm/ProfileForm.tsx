e2a0b1fd68f04d2238b9e802490cd7a6072555a3
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-02-13 16:40:18 +0000

    update/minor-ui-updates

---
+++ frontend/src/components/private/ProfileForm/ProfileForm.tsx
@@ -0,0 +1,39 @@
+import { Input } from "@/components/ui/input";
+import { Field, FieldLabel, FieldDescription } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { useAuth } from "@/hooks/useAuth";
+import { useNavigate } from "react-router";
+import { useMutation } from "@tanstack/react-query";
+
+const ProfileForm = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="w-full p-4 border border-neutral-800 rounded-xl flex flex-col items-start justify-between gap-4">
+        <form className="w-full flex flex-col lg:flex-row items-start justify-between gap-4">
+          <Field>
+            <FieldLabel htmlFor="username">Username</FieldLabel>
+            <Input id="username" type="text" value={user?.name} />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input id="email" type="email" value={user?.email} />
+          </Field>
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input id="password" type="password" placeholder="********" />
+            <FieldDescription>Leave empty to keep current password.</FieldDescription>
+          </Field>
+        </form>
+        <Button type="submit" className="w-full hover:cursor-pointer">
+          Update Account
+          <ArrowRight />
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default ProfileForm;
---
+++ frontend/src/components/private/ProfileUsage/ProfileUsage.tsx
@@ -0,0 +1,33 @@
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Progress } from "@/components/ui/progress";
+import { useAuth } from "@/hooks/useAuth";
+
+const ProfileUsage = () => {
+  const { user } = useAuth();
+  return (
+    <>
+      <div className="w-full p-4 border border-neutral-800 rounded-xl flex flex-col items-start justify-between gap-4">
+        <h2>Your daily usage</h2>
+        <Field className="w-full">
+          <FieldLabel htmlFor="progress-upload" className="-mb-1 text-xs text-neutral-400">
+            <span>Reviews generated today</span>
+            <span className="ml-auto">{user?.dailyReviewsGenerated}/10</span>
+          </FieldLabel>
+          <Progress
+            value={(user?.dailyReviewsGenerated! / 10) * 100}
+            id="progress-upload"
+            className="h-1.5 [&>div]:bg-blue-500"
+          />
+        </Field>
+        <p className="text-xs text-neutral-400">
+          Want more review credits?{" "}
+          <a className="underline italic" href="#">
+            Get in touch with us
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ProfileUsage;
